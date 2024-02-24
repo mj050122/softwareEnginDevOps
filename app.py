ed8c4401905ad594c2be0223a36db07743f0a193
mj050122 <98959924+mj050122@users.noreply.github.com>
2024-02-24 15:30:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 import re
-#adding this here to test pipeline
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
